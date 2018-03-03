85cb2f51d0549c1ed4125e05c443a1a7fa7e7bda
Antoine <zertaps@gmail.com>
2018-03-03 11:18:29 +0100

    Remove fragment content and description. Add static content in app div for crawler.

--- assets/header.php
+++ assets/header.php
@@ -12,9 +12,7 @@
         </script>
         <meta charset="utf8" />
         <title>La BiblioTech - Tesner Antoine</title>
-        <meta name="fragment" content="!">  
         <meta name="keywords" content="Blockchain Javascript">
-        <meta name="description" content="Blockchain, Javascript... J'écris sur ce que j'apprend et ce qui me passionne dans le développement informatique">
         <meta name="viewport" content="width=device-width">
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <?php wp_head(); ?>
